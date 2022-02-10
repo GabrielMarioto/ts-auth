46917669512e5c9841345f993cc4a56fc04ac449
Gabriel Marioto <camilomarioto@hotmail.com>
2022-02-10 14:25:52 -0300

    criado rota de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController";
+import AuthController from "./app/controllers/AuthController";
 
 const routes = Router();
 
 routes.post("/users", UserController.store);
+routes.post("/auth", AuthController.authenticate);
 
 export { routes };
